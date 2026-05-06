cc75b12aaf76a49e6c37be4587f9d0fdce2fe745
seacrab808 <seacrab808@naver.com>
2026-05-06 14:31:13 +0900

    [CT] 왔다 갔던 구역 2

--- codetree/trail_2/chapter4/lesson2_Notation/test_prob.py
+++ codetree/trail_2/chapter4/lesson2_Notation/test_prob.py
@@ -20,3 +20,36 @@ for d in digits[::-1]:
     print(d, end="")
 
 
+# 왔다 갔던 구역 2
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+dict = {}
+index = 0
+
+for i in range(n):
+    if dir[i] == "L":
+        # 음수일 경우
+        # 현재 인덱스에서 계산한 인덱스만큼의 거리 arr에  + 1
+        for j in range(index - x[i], index):
+            dict[j] = dict.get(j, 0) + 1
+        index -= x[i]
+    elif dir[i] == "R":
+        # 양수일 경우
+        for j in range(index, index + x[i]):
+            dict[j] = dict.get(j, 0) + 1
+        index += x[i]
+
+cnt = 0
+for x in dict.values():
+    if x >= 2:
+        cnt += 1
+
+print(cnt)
+## 모두 양수일 경우 배열도 좋지만, 음수가 포함되었거나 범위가 불명확할 경우 딕셔너리를 쓰는게 좋다는 것을 배움
