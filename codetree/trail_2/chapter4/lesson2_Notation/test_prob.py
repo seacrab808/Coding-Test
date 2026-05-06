a480681634fd022c86a01cc7bf8539fa018c2ce8
seacrab808 <seacrab808@naver.com>
2026-05-06 17:27:52 +0900

    [CT] 흰검 칠하기

--- codetree/trail_2/chapter4/lesson2_Notation/test_prob.py
+++ codetree/trail_2/chapter4/lesson2_Notation/test_prob.py
@@ -53,3 +53,93 @@ for x in dict.values():
 
 print(cnt)
 ## 모두 양수일 경우 배열도 좋지만, 음수가 포함되었거나 범위가 불명확할 경우 딕셔너리를 쓰는게 좋다는 것을 배움
+
+
+# 흰검 칠하기
+## 첫번째 시도;;
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+dict = {}
+ans_dict = {}
+index = 0
+
+for i in range(n):
+    if dir[i] == "L":
+        # 음수 방향일 경우
+        for j in range(index - x[i], index):
+            dict[j] = dict.get(j, "") + "W"
+            ans_dict[j] = "W"
+
+            if dict[j].count("W") >= 2 and dict[j].count("B") >= 2:
+                ans_dict[j] = "G"
+        index -= x[i]
+    elif dir[i] == "R":
+        # 양수 방향일 경우
+        for j in range(index, index + x[i]):
+            dict[j] = dict.get(j, "") + "B"
+
+            ans_dict[j] = "B"
+
+            if dict[j].count("W") >= 2 and dict[j].count("B") >= 2:
+                ans_dict[j] = "G"
+        index += x[i]
+
+W_cnt = 0
+B_cnt = 0
+G_cnt = 0
+
+for x in ans_dict.values():
+    if x == "W":
+        W_cnt += 1
+    elif x == "B":
+        B_cnt += 1
+    elif x == "G":
+        G_cnt += 1    
+
+print(W_cnt, B_cnt, G_cnt)
+
+
+##두번째,, (도움)
+n = int(input())
+commands = [tuple(input().split()) for _ in range(n)]
+x = []
+dir = []
+for num, direction in commands:
+    x.append(int(num))
+    dir.append(direction)
+
+# Please write your code here.
+dict = {}
+index = 0
+
+for i in range(n):
+    if dir[i] == "L":
+        for j in range(index, index - x[i], -1):
+            dict[j] = dict.get(j, "") + "W"
+        index -= (x[i] - 1)
+        
+    elif dir[i] == "R":
+        for j in range(index, index + x[i]):
+            dict[j] = dict.get(j, "") + "B"
+        index += (x[i] - 1)
+
+W_cnt = 0
+B_cnt = 0
+G_cnt = 0
+
+for colors in dict.values():
+    if colors.count("W") >= 2 and colors.count("B") >= 2:
+        G_cnt += 1
+    elif colors[-1] == "W":
+        W_cnt += 1
+    elif colors[-1] == "B":
+        B_cnt += 1
+
+print(W_cnt, B_cnt, G_cnt)
